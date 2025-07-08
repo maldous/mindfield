94bc35be42cf1ab0b696bc3a486cef204d785df2
Matthew <matthew.aldous@citoplus.com.au>
2025-07-09 01:18:54 +1000

    1751948379

--- database/init/01-init-keycloak.sql
+++ database/init/01-init-keycloak.sql
@@ -1,4 +1,3 @@
--- Create keycloak database and user
 CREATE DATABASE keycloak;
-GRANT ALL PRIVILEGES ON DATABASE keycloak TO lixit;
-ALTER ROLE lixit WITH PASSWORD 'lixit_dev_password';
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO mindfield;
+ALTER ROLE mindfield WITH PASSWORD 'mindfieldt_dev_password';
