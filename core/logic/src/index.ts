eb3d01e915ce4194d4313d5185475841119d5abc
Matthew <matthew.aldous@citoplus.com.au>
2025-07-09 11:53:09 +1000

    1751948560

--- core/logic/src/index.ts
+++ core/logic/src/index.ts
@@ -1,3 +1,2 @@
 export * from './bootstrap/express.js';
-export * from './bootstrap/nest';
 export * from './bootstrap/nest.js';
--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -1,3 +1,3 @@
 import { startNest } from '@mindfield/logic/bootstrap/nest';
 
-startNest();
+startNest(AppModule);
